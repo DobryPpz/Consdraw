71aad73a90001ee23bd80056efb913530fefbf1f
Aleksander <xaleksx960@gmail.com>
2023-03-31 11:45:24 +0200

    implemented list_command()

--- src/programstate.h
+++ src/programstate.h
@@ -29,6 +29,7 @@ void down_command(FILE *fp, char **saveptr, struct context *c);
 void up_command(FILE *fp, char **saveptr, struct context *c);
 void bottom_command(FILE *fp, char **saveptr, struct context *c);
 void top_command(FILE *fp, char **saveptr, struct context *c);
+void list_command(FILE *fp, char **saveptr, struct context *c);
 void write_png_command(FILE *fp, char **saveptr, struct context *c);
 void read_menu(FILE *fp, struct context *c);
 void read_parsing(FILE *fp, struct context *c);
