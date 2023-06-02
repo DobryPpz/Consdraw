04c68b82fa4a24106d6917f1ad832693800115ae
Aleksander <xaleksx960@gmail.com>
2023-06-03 01:02:11 +0200

    first attempt ad circle command implementation

--- src/utilities.c
+++ src/utilities.c
@@ -8,3 +8,12 @@ int max_int(int a, int b){
     if(a>b) return a;
     return b;
 }
+double dist(int x1, int y1, int x2, int y2){
+    return sqrt(pow(x1-x2,2)+pow(y1-y2,2));
+}
+double absl(double x){
+    if(x<0){
+        x *= -1;
+    }
+    return x;
+}
